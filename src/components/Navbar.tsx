bcba4a7bbcb8c58bad36d091a716cf9aaa8463db
DN Josh <61066433+Dumte@users.noreply.github.com>
2025-08-09 22:59:01 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           className="w-6 h-6 md:w-9 md:h-9"
         />
 
-        <p className="text-sm md:block  md:text-md font-medium tracking-wider uppercase">
+        <p className="text-xs md:block  md:text-md font-medium tracking-wider uppercase">
           Elvis{" "}
           <span className="bg-gradient-to-r from-amber-500 to-red-800 text-transparent bg-clip-text">
             boutique
